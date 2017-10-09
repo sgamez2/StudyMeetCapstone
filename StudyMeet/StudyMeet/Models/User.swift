3f8f51c7e1abafcf784c9feb33314be05505977d
sgamez2 <gamezsergio.97@gmail.com>
2017-10-09 14:57:33 -0600

    completed the initializers for UserModel

--- StudyMeet/StudyMeet/Models/User.swift
+++ StudyMeet/StudyMeet/Models/User.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+
+class User {
+    
+    // MARK: - Private Keys
+    private let firstNameKey = "firstName"
+    private let lastNameKey = "lastName"
+    private let bioKey = "bio"
+    private let emailKey = "email"
+    private let phoneNumberKey = "phoneNumber"
+    private let schoolNameKey = "schoolName"
+    private let userNameKey = "userName"
+    private let uuidKey = "uuid"
+    
+    // MARK: - Properties
+    let firstName: String
+    let lastName: String
+    let bio: String
+    let email: String
+    let phoneNumber: Int?
+    let schoolName: String
+    let userName: String
+    let identifier: UUID
+    
+    // Memberwise INIT
+    init(firstName: String, lastName: String, bio: String, email: String, phoneNumber: Int?, schoolName: String, userName: String, identifier: UUID = UUID()) {
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.bio = bio
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.schoolName = schoolName
+        self.userName = userName
+        self.identifier = identifier
+    }
+    
+    // Failable INIT
+    init?(userDictionary: [String:Any], identifier: String) {
+        guard let firstName = userDictionary[firstNameKey] as? String,
+        let lastName = userDictionary[lastNameKey] as? String,
+        let bio = userDictionary[bioKey] as? String,
+        let email = userDictionary[emailKey] as? String,
+        let phoneNumber = userDictionary[phoneNumberKey] as? Int,
+        let schoolName = userDictionary[schoolNameKey] as? String,
+        let userName = userDictionary[userNameKey] as? String,
+        let uuid = UUID(uuidString: identifier) else {return nil}
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.bio = bio
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.schoolName = schoolName
+        self.userName = userName
+        self.identifier = uuid
+    }
+    
+    
+    
+    
+    
+    
+}
